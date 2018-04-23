795261a091b74f11bee93a8ef3668570eee65da6
Brandon Fuller <bjcfuller@uri.edu>
2018-04-23 10:00:59 -0400

    Enqueue Google code prettify script

--- functions.php
+++ functions.php
@@ -19,5 +19,13 @@ function uri_modern_styleguide_enqueues() {
 	wp_enqueue_style( 'uri-modern-styleguide-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ), wp_get_theme()->get( 'Version' ) );
 
 	wp_enqueue_script( 'uri-modern-styleguide-script', get_stylesheet_directory_uri() . '/js/script.min.js', array(), wp_get_theme()->get( 'Version' ) );
+
+	/**
+	 * Enqueue Google Code Prettify
+	 *
+	 * @link https://github.com/google/code-prettify
+	 */
+	wp_enqueue_script( 'google-code-prettify', 'https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js' );
+
 }
 add_action( 'wp_enqueue_scripts', 'uri_modern_styleguide_enqueues' );
